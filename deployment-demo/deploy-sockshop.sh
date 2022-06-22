4ffd82e2d48e7e4153e63523407898f43bbfff25
Kev Leng <kevin.leng@dynatrace.com>
2022-06-22 13:48:54 +1000

    Update deploy-sockshop.sh

--- deployment-demo/deploy-sockshop.sh
+++ deployment-demo/deploy-sockshop.sh
@@ -3,4 +3,11 @@
 touch /tmp/finished2
 
 git clone https://github.com/KevLeng/sockshop
+
+git clone https://github.com/kubernetes-sigs/sig-storage-local-static-provisioner
+
+curl -fsSL -o get_helm.sh https://raw.githubusercontent.com/helm/helm/main/scripts/get-helm-3
+chmod 700 get_helm.sh
+./get_helm.sh
+
 #cd sockshop
